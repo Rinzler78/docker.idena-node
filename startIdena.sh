9628cbad2e5818eed567536388839db031186699
Boris LECLERE <Borisleclere@gmail.com>
2020-05-23 16:11:25 +0200

    Clean traces Create config.json if not exist

--- startIdena.sh
+++ startIdena.sh
@@ -2,18 +2,20 @@ echo "idena-node start"
 echo "user $IDENA_USER_ACCOUNT_NAME"
 echo "pass $IDENA_USER_ACCOUNT_PASS"
 
-if [ $(getent passwd $IDENA_USER_ACCOUNT_NAME) ] ; then
-    echo "The user exists"
-else
-    echo "The user does not exist => Creating $IDENA_USER_ACCOUNT_NAME"
+if [ ! $(getent passwd $IDENA_USER_ACCOUNT_NAME) ] ; then
+    echo "Creating user : $IDENA_USER_ACCOUNT_NAME"
     useradd -ms /bin/bash ${IDENA_USER_ACCOUNT_NAME}
     echo ${IDENA_USER_ACCOUNT_NAME}:${IDENA_USER_ACCOUNT_PASS} | chpasswd
     usermod -aG sudo ${IDENA_USER_ACCOUNT_NAME}
-    echo "The user does not exist => $IDENA_USER_ACCOUNT_NAME Created"
 fi
 
 service ssh start
 
-echo "Current date : $(date)"
+configFile=/datadir/config.json
+
+if [ ! -f "$configFile" ]; then
+    echo "Creating $configFile"
+    echo '{ "IpfsConf": { "Profile": "server" } }' > /datadir/config.json
+fi
 
 idena-node --config=/datadir/config.json
