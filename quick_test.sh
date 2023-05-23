1e29c9ceac06875cb450a6ade16329259dfb0329
Boris LECLERE <Borisleclere@gmail.com>
2023-05-23 17:42:47 +0200

    Improve quick test to use specified tag name

--- quick_test.sh
+++ quick_test.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-docker build -t idena-node . && docker run --env RUN_TIMEOUT=3 --rm --name idena-node idena-node
+name=$1
 
+if [ -z "$name" ];
+then
+    name="idena-node"
+fi
+
+# Build specified tag and run for 3s
+docker build . --tag $name && docker run --env RUN_TIMEOUT=3 --rm $name
+
+# Catch error code. 124 is success code
 returnedCode=$?
 
 if [ $returnedCode -eq 124 ]; then
